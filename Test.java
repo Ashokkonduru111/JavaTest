ef36dc39acc40e8143067e2dd2fec4cedfa9aa99
Ashokkonduru111 <157702728+Ashokkonduru111@users.noreply.github.com>
2026-07-29 23:58:15 +0530

    Fix formatting issues in Test.java

--- Test.java
+++ Test.java
@@ -3,6 +3,8 @@ public static void main(String[] args) {
     
     for (int i = 0; i < 10; i++) {
         System.out.println("Hello, World! " + i);
+      
     }
+      System.out.println("End Of the LOOP");
 }
 }
